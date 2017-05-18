e0e5d23a23b08e7d32e5f19ec515fb94a48024d2
crystalDf <chendong333@gmail.com>
2017-05-18 17:14:10 +0800

    Chapter 07 SQLiteBestPractice

---
+++ app/src/main/java/com/star/sqlitebestpractice/MainActivity.java
@@ -0,0 +1,170 @@
+package com.star.sqlitebestpractice;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    public static final String DATABASE_BOOK_STORE = "BookStore.db";
+
+    private MyDatabaseHelper mMyDatabaseHelper;
+
+    private Button mCreateDatabaseButton;
+    private Button mInsertDataButton;
+    private Button mUpdateDataButton;
+    private Button mDeleteDataButton;
+    private Button mQueryDataButton;
+    private Button mReplaceDataButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMyDatabaseHelper = new MyDatabaseHelper(this, DATABASE_BOOK_STORE, null, 3);
+
+        mCreateDatabaseButton = (Button) findViewById(R.id.create_database);
+        mCreateDatabaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMyDatabaseHelper.getWritableDatabase();
+            }
+        });
+
+        mInsertDataButton = (Button) findViewById(R.id.insert_data);
+        mInsertDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                ContentValues contentValues = new ContentValues();
+
+                contentValues.put(MyDatabaseHelper.COLUMN_NAME, "The Da Vinci Code");
+                contentValues.put(MyDatabaseHelper.COLUMN_AUTHOR, "Dan Brown");
+                contentValues.put(MyDatabaseHelper.COLUMN_PAGES, 454);
+                contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 16.96);
+
+                sqLiteDatabase.insert(MyDatabaseHelper.TABLE_BOOK, null, contentValues);
+
+                contentValues.clear();
+
+                contentValues.put(MyDatabaseHelper.COLUMN_NAME, "The Lost Symbol");
+                contentValues.put(MyDatabaseHelper.COLUMN_AUTHOR, "Dan Brown");
+                contentValues.put(MyDatabaseHelper.COLUMN_PAGES, 510);
+                contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 19.95);
+
+                sqLiteDatabase.insert(MyDatabaseHelper.TABLE_BOOK, null, contentValues);
+
+            }
+        });
+
+        mUpdateDataButton = (Button) findViewById(R.id.update_data);
+        mUpdateDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                ContentValues contentValues = new ContentValues();
+
+                contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 10.99);
+
+                sqLiteDatabase.update(MyDatabaseHelper.TABLE_BOOK, contentValues,
+                        MyDatabaseHelper.COLUMN_NAME + " = ?", new String[] {"The Da Vinci Code"});
+            }
+        });
+
+        mDeleteDataButton = (Button) findViewById(R.id.delete_data);
+        mDeleteDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                sqLiteDatabase.delete(MyDatabaseHelper.TABLE_BOOK,
+                        MyDatabaseHelper.COLUMN_PAGES + " > ?", new String[] {"500"});
+            }
+        });
+
+        mQueryDataButton = (Button) findViewById(R.id.query_data);
+        mQueryDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                Cursor cursor = sqLiteDatabase.query(MyDatabaseHelper.TABLE_BOOK,
+                        null, null, null, null, null, null);
+
+                if (cursor.moveToFirst()) {
+
+                    while (true) {
+
+                        String name = cursor.getString(cursor.getColumnIndex(
+                                MyDatabaseHelper.COLUMN_NAME));
+                        String author = cursor.getString(cursor.getColumnIndex(
+                                MyDatabaseHelper.COLUMN_AUTHOR));
+                        int pages = cursor.getInt(cursor.getColumnIndex(
+                                MyDatabaseHelper.COLUMN_PAGES));
+                        double price = cursor.getDouble(cursor.getColumnIndex(
+                                MyDatabaseHelper.COLUMN_PRICE));
+
+                        Log.d(TAG, "Book name is " + name);
+                        Log.d(TAG, "Book author is " + author);
+                        Log.d(TAG, "Book pages is " + pages);
+                        Log.d(TAG, "Book price is " + price);
+
+                        if (!cursor.moveToNext()) {
+                            break;
+                        }
+                    }
+                }
+
+                cursor.close();
+            }
+        });
+
+        mReplaceDataButton = (Button) findViewById(R.id.replace_data);
+        mReplaceDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqLiteDatabase = mMyDatabaseHelper.getWritableDatabase();
+
+                sqLiteDatabase.beginTransaction();
+
+                try {
+                    sqLiteDatabase.delete(MyDatabaseHelper.TABLE_BOOK, null, null);
+                    if (false) {
+                        throw new NullPointerException();
+                    }
+
+                    ContentValues contentValues = new ContentValues();
+
+                    contentValues.put(MyDatabaseHelper.COLUMN_NAME, "Game of Thrones");
+                    contentValues.put(MyDatabaseHelper.COLUMN_AUTHOR, "George Martin");
+                    contentValues.put(MyDatabaseHelper.COLUMN_PAGES, 720);
+                    contentValues.put(MyDatabaseHelper.COLUMN_PRICE, 20.85);
+
+                    sqLiteDatabase.insert(MyDatabaseHelper.TABLE_BOOK, null, contentValues);
+
+                    sqLiteDatabase.setTransactionSuccessful();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    sqLiteDatabase.endTransaction();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/star/sqlitebestpractice/MyDatabaseHelper.java
@@ -0,0 +1,68 @@
+package com.star.sqlitebestpractice;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_BOOK = "Book";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_PRICE = "price";
+    public static final String COLUMN_PAGES = "pages";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_FOREIGN_CATEGORY_ID = "category_id";
+
+    public static final String TABLE_CATEGORY = "Category";
+    public static final String COLUMN_CATEGORY_ID = COLUMN_ID;
+    public static final String COLUMN_CATEGORY_NAME = "category_name";
+    public static final String COLUMN_CATEGORY_CODE = "category_code";
+
+    public static final String CREATE_BOOK = "create table " + TABLE_BOOK + " (" +
+            COLUMN_ID + " integer primary key autoincrement" + ", " +
+            COLUMN_AUTHOR + " text" + ", " +
+            COLUMN_PRICE + " real" + ", " +
+            COLUMN_PAGES + " integer" + ", " +
+            COLUMN_NAME + " text" + ", " +
+            COLUMN_FOREIGN_CATEGORY_ID + " integer" +
+            ")";
+
+    public static final String CREATE_CATEGORY = "create table " + TABLE_CATEGORY + " (" +
+            COLUMN_CATEGORY_ID + " integer primary key autoincrement" + ", " +
+            COLUMN_CATEGORY_NAME + " text" + ", " +
+            COLUMN_CATEGORY_CODE + " integer" +
+            ")";
+
+    public static final String ALTER_BOOK = "alter table " + TABLE_BOOK + " add column " +
+            COLUMN_FOREIGN_CATEGORY_ID + " integer";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        switch (oldVersion) {
+            case 1:
+                db.execSQL(CREATE_CATEGORY);
+            case 2:
+                db.execSQL(ALTER_BOOK);
+            default:
+        }
+    }
+}
